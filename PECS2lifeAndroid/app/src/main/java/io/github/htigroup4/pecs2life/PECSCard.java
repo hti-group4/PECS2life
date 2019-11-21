99362acd8a4cf3245d2dedf74d13a3c6305b3659
areee <10089872+areee@users.noreply.github.com>
2019-11-21 16:17:31 +0200

    added a PECSCard object

---
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/PECSCard.java
@@ -0,0 +1,49 @@
+
+
+package io.github.htigroup4.pecs2life;
+
+/**
+ * Data model for each row of the RecyclerView
+ */
+class PECSCard {
+
+    // Member variables representing the title and information about the PECSCard.
+    private String title;
+    private String info;
+    private final int imageResource;
+
+    /**
+     * Constructor for the PECSCard data model.
+     *
+     * @param title The name if the PECSCard.
+     * @param info  Information about the PECSCard.
+     */
+    public PECSCard(String title, String info, int imageResource) {
+        this.title = title;
+        this.info = info;
+        this.imageResource = imageResource;
+    }
+
+    /**
+     * Gets the title of the PECSCard.
+     *
+     * @return The title of the PECSCard.
+     */
+    String getTitle() {
+        return title;
+    }
+
+    /**
+     * Gets the info about the PECSCard.
+     *
+     * @return The info about the PECSCard.
+     */
+    String getInfo() {
+        return info;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+}

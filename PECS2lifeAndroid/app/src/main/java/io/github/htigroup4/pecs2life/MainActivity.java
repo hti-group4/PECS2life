f1a62266f4a32da60b4b2009770486b5cef91329
areee <10089872+areee@users.noreply.github.com>
2019-11-21 11:08:24 +0200

    1: Added (+ modified) tab fragment files from TabExperiment sample to make implementing the tab functionality easier. 2: Removed comments about floatingActionButton

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MainActivity.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MainActivity.java
@@ -21,20 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
 }

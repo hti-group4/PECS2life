678297382875da193715564fa0bc72e0168f5924
areee <10089872+areee@users.noreply.github.com>
2019-11-21 19:33:31 +0200

    added PECSCardAdapter + updated 1st tab fragment

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment1.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment1.java
@@ -19,15 +19,24 @@ package io.github.htigroup4.pecs2life;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TabFragment1 extends Fragment {
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<PECSCard> mPECSCardData;
+
 
     public TabFragment1() {
         // Required empty public constructor

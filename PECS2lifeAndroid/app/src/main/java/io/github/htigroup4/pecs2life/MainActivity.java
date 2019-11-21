bbc019c24c915ea1d97710c08de134d2c78a6e6c
areee <10089872+areee@users.noreply.github.com>
2019-11-21 12:55:20 +0200

    removed the app toolbar & added a photo for the help card button

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MainActivity.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MainActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package io.github.htigroup4.pecs2life;
 
@@ -20,7 +5,6 @@ import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 
@@ -42,9 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         // Create an instance of the tab layout from the view.
         TabLayout tabLayout = findViewById(R.id.tab_layout);

91514cceae2dbf369122246600a57d1db6a8c22f
areee <10089872+areee@users.noreply.github.com>
2019-11-22 20:57:01 +0200

    commented card code that affects errors

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MainActivity.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MainActivity.java
@@ -77,82 +77,82 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        // Initialize the RecyclerView.
-        mRecyclerView = findViewById(R.id.recyclerView);
-
-        // Get the appropriate column count.
-        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
-
-        // Set the Layout Manager.
-        mRecyclerView.setLayoutManager(new GridLayoutManager(
-                this, gridColumnCount));
-
-        // Initialize the ArrayList that will contain the data.
-        mPECSCardData = new ArrayList<>();
-
-        // Initialize the adapter and set it to the RecyclerView.
-        mAdapter = new PECSCardAdapter(this, mPECSCardData);
-        mRecyclerView.setAdapter(mAdapter);
-
-        // Get the data.
-        initializeData();
-
-        // If there is more than one column, disable swipe to dismiss
-        int swipeDirs;
-        if(gridColumnCount > 1){
-            swipeDirs = 0;
-        } else {
-            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
-        }
-
-        // Helper class for creating swipe to dismiss and drag and drop
-        // functionality
-        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
-                .SimpleCallback(
-                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
-                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,
-                swipeDirs) {
-            /**
-             * Defines the drag and drop functionality.
-             *
-             * @param recyclerView The RecyclerView that contains the list items.
-             * @param viewHolder The SportsViewHolder that is being moved.
-             * @param target The SportsViewHolder that you are switching the
-             *               original one with.
-             * @return returns true if the item was moved, false otherwise
-             */
-            @Override
-            public boolean onMove(RecyclerView recyclerView,
-                                  RecyclerView.ViewHolder viewHolder,
-                                  RecyclerView.ViewHolder target) {
-                // Get the from and to positions.
-                int from = viewHolder.getAdapterPosition();
-                int to = target.getAdapterPosition();
-
-                // Swap the items and notify the adapter.
-                Collections.swap(mPECSCardData, from, to);
-                mAdapter.notifyItemMoved(from, to);
-                return true;
-            }
-
-            /**
-             * Defines the swipe to dismiss functionality.
-             *
-             * @param viewHolder The viewholder being swiped.
-             * @param direction The direction it is swiped in.
-             */
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder,
-                                 int direction) {
-                // Remove the item from the dataset.
-                mPECSCardData.remove(viewHolder.getAdapterPosition());
-                // Notify the adapter.
-                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-            }
-        });
-
-        // Attach the helper to the RecyclerView.
-        helper.attachToRecyclerView(mRecyclerView);
+//        // Initialize the RecyclerView.
+//        mRecyclerView = findViewById(R.id.recyclerView);
+//
+//        // Get the appropriate column count.
+//        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
+//
+//        // Set the Layout Manager.
+//        mRecyclerView.setLayoutManager(new GridLayoutManager(
+//                this, gridColumnCount));
+//
+//        // Initialize the ArrayList that will contain the data.
+//        mPECSCardData = new ArrayList<>();
+//
+//        // Initialize the adapter and set it to the RecyclerView.
+//        mAdapter = new PECSCardAdapter(this, mPECSCardData);
+//        mRecyclerView.setAdapter(mAdapter);
+//
+//        // Get the data.
+//        initializeData();
+//
+//        // If there is more than one column, disable swipe to dismiss
+//        int swipeDirs;
+//        if(gridColumnCount > 1){
+//            swipeDirs = 0;
+//        } else {
+//            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+//        }
+//
+//        // Helper class for creating swipe to dismiss and drag and drop
+//        // functionality
+//        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
+//                .SimpleCallback(
+//                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
+//                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,
+//                swipeDirs) {
+//            /**
+//             * Defines the drag and drop functionality.
+//             *
+//             * @param recyclerView The RecyclerView that contains the list items.
+//             * @param viewHolder The SportsViewHolder that is being moved.
+//             * @param target The SportsViewHolder that you are switching the
+//             *               original one with.
+//             * @return returns true if the item was moved, false otherwise
+//             */
+//            @Override
+//            public boolean onMove(RecyclerView recyclerView,
+//                                  RecyclerView.ViewHolder viewHolder,
+//                                  RecyclerView.ViewHolder target) {
+//                // Get the from and to positions.
+//                int from = viewHolder.getAdapterPosition();
+//                int to = target.getAdapterPosition();
+//
+//                // Swap the items and notify the adapter.
+//                Collections.swap(mPECSCardData, from, to);
+//                mAdapter.notifyItemMoved(from, to);
+//                return true;
+//            }
+//
+//            /**
+//             * Defines the swipe to dismiss functionality.
+//             *
+//             * @param viewHolder The viewholder being swiped.
+//             * @param direction The direction it is swiped in.
+//             */
+//            @Override
+//            public void onSwiped(RecyclerView.ViewHolder viewHolder,
+//                                 int direction) {
+//                // Remove the item from the dataset.
+//                mPECSCardData.remove(viewHolder.getAdapterPosition());
+//                // Notify the adapter.
+//                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+//            }
+//        });
+//
+//        // Attach the helper to the RecyclerView.
+//        helper.attachToRecyclerView(mRecyclerView);
     }
 
     public void sendHelpRequest(View view) {
@@ -161,32 +161,32 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    /**
-     * Initialize the PECSCard data from resources.
-     */
-    private void initializeData() {
-        // Get the resources from the XML file.
-        String[] sportsList = getResources()
-                .getStringArray(R.array.food_titles);
-        String[] sportsInfo = getResources()
-                .getStringArray(R.array.food_info);
-        TypedArray sportsImageResources =
-                getResources().obtainTypedArray(R.array.food_images);
-
-        // Clear the existing data (to avoid duplication).
-        mPECSCardData.clear();
-
-        // Create the ArrayList of Sports objects with the titles and
-        // information about each sport.
-        for (int i = 0; i < sportsList.length; i++) {
-            mPECSCardData.add(new PECSCard(sportsList[i], sportsInfo[i],
-                    sportsImageResources.getResourceId(i, 0)));
-        }
-
-        // Recycle the typed array.
-        sportsImageResources.recycle();
-
-        // Notify the adapter of the change.
-        mAdapter.notifyDataSetChanged();
-    }
+//    /**
+//     * Initialize the PECSCard data from resources.
+//     */
+//    private void initializeData() {
+//        // Get the resources from the XML file.
+//        String[] sportsList = getResources()
+//                .getStringArray(R.array.food_titles);
+//        String[] sportsInfo = getResources()
+//                .getStringArray(R.array.food_info);
+//        TypedArray sportsImageResources =
+//                getResources().obtainTypedArray(R.array.food_images);
+//
+//        // Clear the existing data (to avoid duplication).
+//        mPECSCardData.clear();
+//
+//        // Create the ArrayList of Sports objects with the titles and
+//        // information about each sport.
+//        for (int i = 0; i < sportsList.length; i++) {
+//            mPECSCardData.add(new PECSCard(sportsList[i], sportsInfo[i],
+//                    sportsImageResources.getResourceId(i, 0)));
+//        }
+//
+//        // Recycle the typed array.
+//        sportsImageResources.recycle();
+//
+//        // Notify the adapter of the change.
+//        mAdapter.notifyDataSetChanged();
+//    }
 }

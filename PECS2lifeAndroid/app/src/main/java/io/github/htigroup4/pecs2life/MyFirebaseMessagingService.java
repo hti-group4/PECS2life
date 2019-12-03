25c40e796655e5745fedb4d539abf18f8b5be322
areee <10089872+areee@users.noreply.github.com>
2019-12-03 14:07:41 +0200

    updated MyFirebaseMessagingService (it's ready now)

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MyFirebaseMessagingService.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/MyFirebaseMessagingService.java
@@ -1,16 +1,81 @@
 package io.github.htigroup4.pecs2life;
 
-import android.app.Service;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 
-public class MyFirebaseMessagingService extends Service {
-    public MyFirebaseMessagingService() {
-    }
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private final String HELP_REQUEST_CHANNEL_ID = "help_request_channel";
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        int notificationID = new Random().nextInt(3000);
+
+        /*
+        Apps targeting SDK 26 or above (Android O) must implement notification channels and add its notifications
+        to at least one of them.
+      */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            assert notificationManager != null;
+            setupChannels(notificationManager);
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_person);
+        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, HELP_REQUEST_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_person)
+                .setLargeIcon(largeIcon)
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("message"))
+                .setAutoCancel(true)
+                .setSound(notificationSoundUri)
+                .setContentIntent(pendingIntent);
+
+        //Set notification color to match your app color template
+        notificationBuilder.setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+        assert notificationManager != null;
+        notificationManager.notify(notificationID, notificationBuilder.build());
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(NotificationManager notificationManager) {
+        CharSequence channelName = getString(R.string.notification_channel_name);
+        String channelDescription = getString(R.string.notification_channel_description);
+
+        NotificationChannel channel = new NotificationChannel(HELP_REQUEST_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        channel.setDescription(channelDescription);
+        channel.enableLights(true);
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+
 }

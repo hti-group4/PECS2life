a3e48bd147684491312fa2808db2db48b196f9d2
areee <10089872+areee@users.noreply.github.com>
2019-12-11 12:30:44 +0200

    TabFragment1: removed unnecessary codes. TabFragment2: added swipe to dismiss feature & onClick feature for floatingActionButton

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment2.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment2.java
@@ -6,15 +6,20 @@ package io.github.htigroup4.pecs2life;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 /**
@@ -60,24 +65,24 @@ public class TabFragment2 extends Fragment {
 
         // Helper class for creating swipe to dismiss and drag and drop
         // functionality.
-//        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
-//                .SimpleCallback(
-//                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
-//                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,
-//                0) {
-//            /**
-//             * Defines the drag and drop functionality.
-//             *
-//             * @param recyclerView The RecyclerView that contains the list items
-//             * @param viewHolder The PECSCardViewHolder that is being moved
-//             * @param target The PECSCardViewHolder that you are switching the
-//             *               original one with.
-//             * @return true if the item was moved, false otherwise
-//             */
-//            @Override
-//            public boolean onMove(@NonNull RecyclerView recyclerView,
-//                                  @NonNull RecyclerView.ViewHolder viewHolder,
-//                                  @NonNull RecyclerView.ViewHolder target) {
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
+                .SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT |
+                        ItemTouchHelper.RIGHT) {
+            /**
+             * Defines the drag and drop functionality.
+             *
+             * @param recyclerView The RecyclerView that contains the list items
+             * @param viewHolder The PECSCardViewHolder that is being moved
+             * @param target The PECSCardViewHolder that you are switching the
+             *               original one with.
+             * @return true if the item was moved, false otherwise
+             */
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView,
+                                  @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder target) {
 //                // Get the from and to positions.
 //                int from = viewHolder.getAdapterPosition();
 //                int to = target.getAdapterPosition();
@@ -85,27 +90,36 @@ public class TabFragment2 extends Fragment {
 //                // Swap the items and notify the adapter.
 //                Collections.swap(mPECSCardsData, from, to);
 //                mAdapter.notifyItemMoved(from, to);
-//                return true;
-//            }
-//
-//            /**
-//             * Defines the swipe to dismiss functionality.
-//             *
-//             * @param viewHolder The viewholder being swiped.
-//             * @param direction The direction it is swiped in.
-//             */
-//            @Override
-//            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder,
-//                                 int direction) {
-//                // Remove the item from the dataset.
-//                mPECSCardsData.remove(viewHolder.getAdapterPosition());
-//                // Notify the adapter.
-//                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-//            }
-//        });
-//
-//        // Attach the helper to the RecyclerView.
-//        helper.attachToRecyclerView(mRecyclerView);
+                return false; // was true
+            }
+
+            /**
+             * Defines the swipe to dismiss functionality.
+             *
+             * @param viewHolder The viewholder being swiped.
+             * @param direction The direction it is swiped in.
+             */
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder,
+                                 int direction) {
+                // Remove the item from the dataset.
+                mPECSCardsData.remove(viewHolder.getAdapterPosition());
+                // Notify the adapter.
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+
+        // Attach the helper to the RecyclerView.
+        helper.attachToRecyclerView(mRecyclerView);
+
+
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initializeData();
+            }
+        });
 
         return view;
     }
@@ -134,5 +148,14 @@ public class TabFragment2 extends Fragment {
         //Notify the adapter of the change
         mAdapter.notifyDataSetChanged();
     }
+
+//    /**
+//     * onClick method for th FAB that resets the data.
+//     *
+//     * @param view The button view that was clicked.
+//     */
+//    public void resetSports(View view) {
+//        initializeData();
+//    }
 
 }

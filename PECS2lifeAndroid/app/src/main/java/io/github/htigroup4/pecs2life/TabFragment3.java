cd43572df5deb38006dd26fdd7540f4d9d02e458
areee <10089872+areee@users.noreply.github.com>
2019-12-12 16:45:50 +0200

    finished the training with the "Firebase Database To Android RecyclerView" tutorial (in TabFragment3)

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment3.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment3.java
@@ -5,6 +5,7 @@ package io.github.htigroup4.pecs2life;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +13,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -28,10 +32,23 @@ public class TabFragment3 extends Fragment {
     private ArrayList<DataSetFire> arrayList;
     private FirebaseRecyclerOptions<DataSetFire> options;
     private FirebaseRecyclerAdapter<DataSetFire, FirebaseViewHolder> adapter;
+    private DatabaseReference databaseReference;
 
 
     public TabFragment3() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     @Override
@@ -41,10 +58,31 @@ public class TabFragment3 extends Fragment {
         View view = inflater.inflate(R.layout.tab_fragment3, container, false);
 
         //Initialize the RecyclerView
-        mRecyclerView = view.findViewById(R.id.recyclerView2);
+        mRecyclerView = view.findViewById(R.id.recyclerView3);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         arrayList = new ArrayList<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("cwc");
+        databaseReference.keepSynced(true);
+        options = new FirebaseRecyclerOptions.Builder<DataSetFire>().setQuery(databaseReference, DataSetFire.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<DataSetFire, FirebaseViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseViewHolder holder, int position, @NonNull DataSetFire model) {
+                holder.teamone.setText(model.getTeamone());
+                holder.teamtwo.setText(model.getTeamtwo());
+                holder.itemView.setOnClickListener(view1 -> Toast.makeText(getContext(), "teamone: " + model.getTeamone() + ", teamtwo: " + model.getTeamtwo(), Toast.LENGTH_SHORT).show());
+            }
+
+            @NonNull
+            @Override
+            public FirebaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new FirebaseViewHolder(LayoutInflater.from(getContext()).inflate(R.layout.row, parent, false));
+            }
+        };
+
+        mRecyclerView.setAdapter(adapter);
+
         return view;
     }
 

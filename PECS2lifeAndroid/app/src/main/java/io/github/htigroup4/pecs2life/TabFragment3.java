f152c04ad7f8f4eb8842701d0d1570fbb6a575cd
areee <10089872+areee@users.noreply.github.com>
2019-12-13 12:15:46 +0200

    a bit cleaning & refactoring in the 3rd tab (the music card feature)

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment3.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment3.java
@@ -76,7 +76,7 @@ public class TabFragment3 extends Fragment {
 
         arrayList = new ArrayList<>();
         for (int i = 1; i <= 4; i++) {
-            arrayList.add(new MusicData("Laulu " + i));
+            arrayList.add(new MusicData(getString(R.string.song_string) + " " + i));
         }
 
 
@@ -108,12 +108,21 @@ public class TabFragment3 extends Fragment {
         // set the adapter to the RecyclerView
         mRecyclerView.setAdapter(adapter);
 
+        // If there is more than one column, disable swipe to dismiss
+        int swipeDirs;
+
+        if (gridColumnCount > 1) {
+            swipeDirs = 0;
+        } else {
+            swipeDirs = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+        }
+
         // Helper class for creating swipe to dismiss and drag and drop
         // functionality.
         ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
                 .SimpleCallback(
                 0,
-                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                swipeDirs) {
 
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView,
@@ -131,12 +140,7 @@ public class TabFragment3 extends Fragment {
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder,
                                  int direction) {
-
-                //String[] songs = {"Laulu 1", "Laulu 2", "Laulu 3", "Laulu 4"};
-
-                // get the position of the card: e.g. 0th is the first one
-                //int position = viewHolder.getAdapterPosition();
-
+                // get the index of the last card
                 int i = arrayList.size() - 1;
 
                 DatabaseReference child = databaseReference.child("song" + i);
@@ -145,20 +149,8 @@ public class TabFragment3 extends Fragment {
 
                 arrayList.remove(i);
 
-                // 0th position -> song[last]
-
-
-                // Remove the item from the dataset.
-                //DatabaseReference child = databaseReference.child("song" + position);
-                //child.removeValue();
-
                 // Notify the adapter.
                 adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-
-                // Remove the item from the dataset.
-                //mPECSCardsData.remove(viewHolder.getAdapterPosition());
-                // Notify the adapter.
-                //mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
             }
         });
 
@@ -166,34 +158,26 @@ public class TabFragment3 extends Fragment {
 
         // Removes all the music cards and recreates them to the database:
         FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton2);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //String[] songs = {"Laulu 1", "Laulu 2", "Laulu 3", "Laulu 4"};
-                //databaseReference = FirebaseDatabase.getInstance().getReference().child("Music");
+
+        // if the device is NOT a tablet (ie. a mobile device = a teacher device), allow the functionality
+        if (!getResources().getBoolean(R.bool.isTablet)) {
+            floatingActionButton.setOnClickListener(view12 -> {
                 databaseReference.removeValue();
 
                 arrayList.clear();
 
                 for (int i = 1; i <= 4; i++) {
-                    MusicData musicData = new MusicData("Laulu " + i);
+                    MusicData musicData = new MusicData(getString(R.string.song_string) + " " + i);
                     arrayList.add(musicData);
 
                     int j = i - 1;
                     DatabaseReference child = databaseReference.child("song" + j);
                     child.setValue(musicData);
-
-
                 }
-
-//                for (int i = 0; i < songs.length; i++) {
-//                    MusicData musicData = new MusicData(songs[i]);
-//                    DatabaseReference child = databaseReference.child("song" + i);
-//                    child.setValue(musicData);
-//                }
-            }
-        });
-
+            });
+        } else {
+            floatingActionButton.hide();
+        }
         return view;
     }
 

c5355e80c59bb8732554018928b7e2ce2795efc2
areee <10089872+areee@users.noreply.github.com>
2019-12-19 10:38:45 +0200

    1: deleted unnecessary MyHolder & Player classes. 2: renamed MyAdapter class into PECSCardAdapter2. 3: added copyright notes and comments. 4: fixed the checkBox bug. 5: a bit cleaning unnecessary code.

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/PECSCardAdapter2.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/PECSCardAdapter2.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) of the original layout file: 2018 Google Inc.
+ * Copyright (C) of the edited file: 2019 hti-group4 (Arttu Ylhävuori, Louis Sosa and Tamilselvi Jayavelu).
+ * Changes made to this file: TODO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.htigroup4.pecs2life;
 
 import android.content.Context;
@@ -16,47 +34,65 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+/***
+ * The adapter class for the RecyclerView, contains the PECSCard data
+ */
+public class PECSCardAdapter2 extends RecyclerView.Adapter<PECSCardAdapter2.ViewHolder> {
 
     //Member variables
     private Context mContext;
     private ArrayList<PECSCard> mPECSCards;
     ArrayList<PECSCard> mCheckedPECSCards;
 
-    MyAdapter(Context context, ArrayList<PECSCard> mPECSCards) {
+    /**
+     * Constructor that passes in the PECSCard data and the context
+     *
+     * @param mPECSCards ArrayList containing the PECSCard data
+     * @param context    Context of the application
+     */
+    PECSCardAdapter2(Context context, ArrayList<PECSCard> mPECSCards) {
         this.mContext = context;
         this.mPECSCards = mPECSCards;
         this.mCheckedPECSCards = new ArrayList<>();
     }
 
+    /**
+     * Required method for creating the viewholder objects.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return The newly create ViewHolder.
+     */
     @NonNull
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        //View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.model, null);
-
+    public PECSCardAdapter2.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.list_item_with_checkbox, parent, false));
     }
 
+    /**
+     * Required method that binds the data to the viewholder.
+     *
+     * @param holder   The viewholder into which the data should be put.
+     * @param position The adapter position.
+     */
     @Override
-    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull PECSCardAdapter2.ViewHolder holder, int position) {
 
         //Get current PECSCard
         PECSCard currentPECSCard = mPECSCards.get(position);
 
         holder.bindTo(currentPECSCard);
 
-        //holder.nameTxt.setText(mPECSCards.get(position).getName());
-        //holder.img.setImageResource(mPECSCards.get(position).getImage());
-
         holder.setItemClickListener((v, pos) -> {
             CheckBox checkBox = (CheckBox) v;
 
             if (checkBox.isChecked()) {
+                holder.setCheckedValueToTrue();
                 mCheckedPECSCards.add(mPECSCards.get(pos));
 
                 System.out.println("DEBUG: an item in position " + pos + " checked");
             } else if (!checkBox.isChecked()) {
+                holder.setCheckedValueToFalse();
                 mCheckedPECSCards.remove(mPECSCards.get(pos));
 
                 System.out.println("DEBUG: an item in position " + pos + " unchecked");
@@ -65,6 +101,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     }
 
+    /**
+     * Required method for determining the size of the data set.
+     *
+     * @return Size of the data set.
+     */
     @Override
     public int getItemCount() {
         return mPECSCards.size();
@@ -78,9 +119,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         //Member Variables for the TextViews
         private TextView mTitleText;
         private ImageView mPECSCardImage;
-        private CheckBox mCheckBox;
 
         private ItemClickListener itemClickListener;
+
+        private boolean mIsChecked;
 
         /**
          * Constructor for the ViewHolder, used in onCreateViewHolder().
@@ -93,19 +135,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             //Initialize the views
             mTitleText = itemView.findViewById(R.id.title2);
             mPECSCardImage = itemView.findViewById(R.id.PECSCardImage2);
-            mCheckBox = itemView.findViewById(R.id.checkBox);
+            CheckBox mCheckBox = itemView.findViewById(R.id.checkBox);
 
             mCheckBox.setOnClickListener(this);
 
-//            itemView.setOnLongClickListener(new View.OnLongClickListener() {
-//                @Override
-//                public boolean onLongClick(View view) {
-//                    return false;
-//                }
-//            });
-
-            // Set the OnClickListener to the entire view.
-            //itemView.setOnClickListener(this);
+            mIsChecked = false;
         }
 
         void bindTo(PECSCard currentPECSCard) {
@@ -130,15 +164,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public void onClick(View view) {
             this.itemClickListener.onItemClick(view, getLayoutPosition());
 
-            Intent arPreviewIntent = new Intent(mContext, ARPreviewActivity.class);
-            arPreviewIntent.putExtra("position", getAdapterPosition());
-            mContext.startActivity(arPreviewIntent);
+            if (this.mIsChecked) {
+                Intent arPreviewIntent = new Intent(mContext, ARPreviewActivity.class);
+                arPreviewIntent.putExtra("position", getAdapterPosition());
+                mContext.startActivity(arPreviewIntent);
+            }
         }
 
+        void setCheckedValueToTrue() {
+            this.mIsChecked = true;
+        }
 
-
-//        public void onLongClick(View view){
-//
-//        }
+        void setCheckedValueToFalse() {
+            this.mIsChecked = false;
+        }
     }
 }
--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment3.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/TabFragment3.java
@@ -27,7 +27,6 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -35,16 +34,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class TabFragment3 extends Fragment {
 
     private StringBuffer sb = null;
-    private MyAdapter adapter;
+    private PECSCardAdapter2 mAdapter;
 
-    private ArrayList<PECSCard> mPECSCardsData; // Player -> PECSCard
-    private RecyclerView mRecyclerView;
-    //private PECSCardAdapter mAdapter;
-
+    private ArrayList<PECSCard> mPECSCardsData;
 
     public TabFragment3() {
         // Required empty public constructor
@@ -58,92 +56,42 @@ public class TabFragment3 extends Fragment {
         View view = inflater.inflate(R.layout.tab_fragment3, container, false);
 
         //Initialize the RecyclerView
-        mRecyclerView = view.findViewById(R.id.recyclerView3);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView3);
 
         // Get the appropriate column count.
         int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
 
         // Set the Layout Manager.
-        mRecyclerView.setLayoutManager(new GridLayoutManager(
+        recyclerView.setLayoutManager(new GridLayoutManager(
                 getContext(), gridColumnCount));
 
         //Initialize the ArrayList that will contain the data
         mPECSCardsData = new ArrayList<>();
 
-        //Initialize the adapter and set it ot the RecyclerView
-        adapter = new MyAdapter(getContext(), mPECSCardsData);
-        mRecyclerView.setAdapter(adapter);
+        //Initialize the mAdapter and set it ot the RecyclerView
+        mAdapter = new PECSCardAdapter2(getContext(), mPECSCardsData);
+        recyclerView.setAdapter(mAdapter);
 
         //Get the data
         initializeData();
-
-        //adapter = new MyAdapter(getContext(), getPlayers());
 
         FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton3);
         floatingActionButton.setOnClickListener(view1 -> {
             sb = new StringBuffer();
 
-            for (PECSCard card : adapter.mCheckedPECSCards) {
+            for (PECSCard card : mAdapter.mCheckedPECSCards) {
                 sb.append(card.getTitle());
                 sb.append("\n");
             }
 
-//            for (Player p : adapter.mCheckedPECSCards) {
-//                sb.append(p.getName());
-//                sb.append("\n");
-//            }
-
-            if (adapter.mCheckedPECSCards.size() > 0) {
+            if (mAdapter.mCheckedPECSCards.size() > 0) {
                 Toast.makeText(getContext(), sb.toString(), Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(getContext(), "Please check PECSCard(s)", Toast.LENGTH_LONG).show();
             }
         });
-
-        //RecyclerView rv = view.findViewById(R.id.recyclerView3);
-
-        //RecyclerView rv2 = view.findViewById(R.id.recyclerViewFooter);
-
-//        int gridColumnCount = getResources().getInteger(R.integer.grid_column_count);
-//
-//        rv.setLayoutManager(new GridLayoutManager(getContext(), gridColumnCount));
-//        rv.setItemAnimator(new DefaultItemAnimator());
-//        rv.setAdapter(adapter);
-
-        //rv2.setLayoutManager(new GridLayoutManager(getContext(), gridColumnCount));
-        //MyAdapter newAdapter = new MyAdapter(getContext(), adapter.mCheckedPECSCards);
-        //rv2.setAdapter(newAdapter);
-
-
         return view;
     }
-
-//    private ArrayList<Player> getPlayers() {
-//        ArrayList<Player> players = new ArrayList<>();
-//
-//        Player p = new Player("Item 1", R.drawable.img_banana);
-//        players.add(p);
-//
-//        p = new Player("Item 2", R.drawable.img_apple);
-//        players.add(p);
-//
-//        p = new Player("Item 3", R.drawable.img_carrot);
-//        players.add(p);
-//
-//        p = new Player("Item 4", R.drawable.img_kebab_meal);
-//        players.add(p);
-//
-//        p = new Player("Item 5", R.drawable.img_red_pepper);
-//        players.add(p);
-//
-//        p = new Player("Item 6", R.drawable.img_sandwich);
-//        players.add(p);
-//
-//        p = new Player("Item 7", R.drawable.img_hamburger);
-//        players.add(p);
-//
-//        return players;
-//    }
 
     /**
      * Method for initializing the PECSCards data from resources.
@@ -166,8 +114,8 @@ public class TabFragment3 extends Fragment {
         // Recycle the typed array.
         PECSCardsImageResources.recycle();
 
-        //Notify the adapter of the change
-        adapter.notifyDataSetChanged();
+        //Notify the mAdapter of the change
+        mAdapter.notifyDataSetChanged();
     }
 
 }

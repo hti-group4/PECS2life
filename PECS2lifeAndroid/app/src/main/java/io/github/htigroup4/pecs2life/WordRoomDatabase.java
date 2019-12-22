395c994e95d4ee606255b9dbbfac0c6d3aeed681
areee <10089872+areee@users.noreply.github.com>
2019-12-22 20:01:22 +0200

    added more example words to the database

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/WordRoomDatabase.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/WordRoomDatabase.java
@@ -50,7 +50,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
         private final WordDao mDao;
-        String[] words = {"Volvo", "Nissan", "Skoda"};
+        String[] words = {"Volvo", "Nissan", "Skoda", "Volkswagen", "Fiat", "Mitsubishi", "Tesla"};
 
         PopulateDbAsync(WordRoomDatabase db) {
             mDao = db.wordDao();

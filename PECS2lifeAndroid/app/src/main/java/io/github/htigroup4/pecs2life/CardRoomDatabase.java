26ce3b2b3ea9098a8966d3641d95cee6907f2d2e
areee <10089872+areee@users.noreply.github.com>
2019-12-23 14:21:06 +0200

    test: populated the database with card titles & images

--- PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/CardRoomDatabase.java
+++ PECS2lifeAndroid/app/src/main/java/io/github/htigroup4/pecs2life/CardRoomDatabase.java
@@ -1,10 +1,13 @@
 package io.github.htigroup4.pecs2life;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Card.class}, version = 1, exportSchema = false)
 public abstract class CardRoomDatabase extends RoomDatabase {
@@ -14,6 +17,7 @@ public abstract class CardRoomDatabase extends RoomDatabase {
     private static CardRoomDatabase INSTANCE;
 
     public static CardRoomDatabase getDatabase(final Context context) {
+
         if (INSTANCE == null) {
             synchronized (CardRoomDatabase.class) {
                 if (INSTANCE == null) {
@@ -23,11 +27,53 @@ public abstract class CardRoomDatabase extends RoomDatabase {
                             // if no Migration object.
                             // Migration is not part of this practical.
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    /**
+     * Populate the database in the background.
+     */
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final CardDao mDao;
+
+        String[] words = {"Red pepper", "Lunch", "Sandwich", "Apple", "Hamburger", "Banana", "Carrot"};
+        int[] images = {R.drawable.img_red_pepper, R.drawable.img_kebab_meal, R.drawable.img_sandwich,
+                R.drawable.img_apple, R.drawable.img_hamburger, R.drawable.img_banana, R.drawable.img_carrot};
+
+
+        PopulateDbAsync(CardRoomDatabase db) {
+            mDao = db.cardDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for (int i = 0; i < words.length; i++) {
+                Card card = new Card(words[i], images[i]);
+                mDao.insert(card);
+            }
+            return null;
+        }
     }
 
 }
